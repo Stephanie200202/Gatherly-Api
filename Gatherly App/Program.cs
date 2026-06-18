b6e37d56404e010cb32114dea5237a84c92f9319
Stephanie <madunaguchinecherem@gmail.com>
2026-06-18 17:52:34 +0100

    Update program

--- Gatherly App/Program.cs
+++ Gatherly App/Program.cs
@@ -1,3 +1,133 @@
+//using System.Text;
+//using System.Security.Claims;
+//using Gatherly.Application.Abstractions;
+//using Gatherly.Application.Configuration;
+//using Gatherly.Application.IServices;
+//using Gatherly.Application.Services;
+//using Gatherly.Applicaton.Configuration;
+//using Gatherly.Applicaton.IServices;
+//using Gatherly.Domain.Interfaces;
+//using Gatherly.Domain.IRepositories;
+//using Gatherly.Infrastructure.Data;
+//using Gatherly.Infrastructure.HelperServices;
+//using Gatherly.Infrastructure.Repositories;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.IdentityModel.Tokens;
+
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.Configure<JwtSettings>(
+//    builder.Configuration.GetSection("Jwt"));
+//builder.Services.Configure<EmailSettings>(
+//    builder.Configuration.GetSection("EmailSettings"));
+//builder.Services.Configure<FileUploadSettings>(
+//    builder.Configuration.GetSection("FileUpload"));
+
+//builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+//    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+//builder.Services.AddScoped<IEventRepository, EventRepository>();
+//builder.Services.AddScoped<IPassRepository, PassRepository>();
+//builder.Services.AddScoped<IVerificationRepository, VerificationRepository>();
+//builder.Services.AddScoped<IRegistrationRepository, RegistrationRepository>();
+//builder.Services.AddScoped<IAnnouncementRepository, AnnouncementRepository>();
+//builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+//builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
+
+//builder.Services.AddScoped<IAuthServices, AuthService>();
+//builder.Services.AddScoped<IRegistrationService, RegistrationService>();
+//builder.Services.AddScoped<IPassService, PassService>();
+//builder.Services.AddScoped<IVerificationService, VerificationService>();
+//builder.Services.AddScoped<INotificationService, NotificationService>();
+//builder.Services.AddScoped<IFeedbackAnalyticsService, FeedbackAnalyticsService>();
+//builder.Services.AddScoped<IEventService, EventService>();
+//builder.Services.AddScoped<IJwtProvider, JwtProvider>();
+//builder.Services.AddHttpContextAccessor();
+//builder.Services.AddScoped<ICurrentUserAbstraction, CurrentUserService>();
+//builder.Services.AddTransient<IEmailService, EmailService>();
+
+//builder.Services.AddAuthentication(a =>
+//{
+//    a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//    a.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//    .AddJwtBearer(options =>
+//    {
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuer = true,
+//            ValidateAudience = true,
+//            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//            ValidAudience = builder.Configuration["Jwt:Audience"],
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!)),
+//            ValidateLifetime = true,
+
+//            //RoleClaimType = "role"
+//        };
+
+//        options.Events = new JwtBearerEvents
+//        {
+//            OnAuthenticationFailed = context =>
+//            {
+//                System.Diagnostics.Debug.WriteLine("JWT Auth Failed: " + context.Exception.Message);
+//                return Task.CompletedTask;
+//            }
+//        };
+//    });
+//builder.Services.AddAuthorization();
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen(options =>
+//{
+
+//    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+//    {
+//        Description = "JWT Authorization header using the Bearer scheme. Paste your raw token values directly below.",
+//        Name = "Authorization",
+//        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+//        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+//        Scheme = "Bearer",
+//        BearerFormat = "JWT"
+//    });
+
+//    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+//    {
+//        {
+//            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+//            {
+//                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+//                {
+//                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+//                    Id = "Bearer"
+//                }
+//            },
+//            Array.Empty<string>()
+//        }
+//    });
+//});
+
+//var app = builder.Build();
+//if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI(c =>
+//    {
+//        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gatherly API v1.0");
+//        c.RoutePrefix = "swagger";
+//    });
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
 using System.Text;
 using System.Security.Claims;
 using Gatherly.Application.Abstractions;
@@ -23,8 +153,15 @@ builder.Services.Configure<EmailSettings>(
 builder.Services.Configure<FileUploadSettings>(
     builder.Configuration.GetSection("FileUpload"));
 
-builder.Services.AddDbContext<ApplicationDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+//    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure();
+        }));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IEventRepository, EventRepository>();
@@ -110,15 +247,15 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gatherly API v1.0");
-        c.RoutePrefix = "swagger";
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gatherly API v1.0");
+    c.RoutePrefix = "swagger";
+});
+
+
 
 app.UseHttpsRedirection();
 
